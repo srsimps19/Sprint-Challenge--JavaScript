e7fd551a5dd8c48f6fa3feb3e25917905422b86b
srsimps19 <srsimps19@gmail.com>
2019-04-05 12:22:03 -0500

    tweaked arrays section on the objects-arrays page and finished functions page

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function hof(param1, param2, cb) {
+  return cb(param1, param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +17,26 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function consume(param1, param2, cb){
+  return cb(param1, param2);
+};
+
+const add = function(param1, param2){
+  return param1 + param2;
+};
+
+const multiply = function(param1, param2) {
+  return param1 * param2;
+};
+
+const greeting = function(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+};
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -27,6 +45,7 @@
 
 // Explanation: 
 
+// `nestedfunction()` can access `internal` because they are both within the scope of `myFunction()`
 
 const external = "I'm outside the function";
 
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -98,9 +98,14 @@ console.log(contactInfo);
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
 for (let i = 0; i < graduates.length; i++) {
-  uni.push(graduates[i].university.search("uni")); 
+    if(graduates[i].university.includes("Uni")){
+      uni.push(graduates[i].university)
+  }
+  
 }
 console.log(uni);
+
+
 
 
 // ==== ADVANCED Array Methods ====
